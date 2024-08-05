bb6a0a9663afba9bb3b7c6176cd7790c14c9a363
mofrim <mofrim@posteo.de>
2024-08-05 19:47:05 +0200

    move printf to seperate subdir

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 11:43:28 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/08/05 19:36:55 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/08/05 19:40:28 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,27 @@ int	ft_printf(const char *fmt, ...)
 	chars_printed = ftpr_parse_args(args, fmt);
 	va_end(args);
 	return (chars_printed);
+}
+
+/*
+ * Checks for INVALID conversions.
+ *
+ * Cases for this: Conversion running off the strings end. To >INT_MAX prec or
+ * width numbers.
+ * 
+ */
+int	ftpr_check_fmt(const char *fmt)
+{
+	while (*fmt)
+	{
+		if (*fmt == '%')
+		{
+			if (ftpr_compl_isconv(fmt) == -1)
+				return (0);
+			while (ftpr_isflagconv(*(++fmt)) != 1)
+				;
+		}
+		fmt++;
+	}
+	return (1);
 }
