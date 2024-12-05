acfc642e455db209e3ae23e45dfb57c29d53c201
mofrim <mofrim@posteo.de>
2024-12-05 18:06:29 +0100

    fix empty string behaviour in ft_strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 11:35:26 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/11/29 11:43:55 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/12/05 18:05:20 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,9 @@ int	ft_strcmp(char	*s1, char *s2)
 		return (0);
 	if ((!s1 && s2) || (s1 && !s2))
 		return (1);
+	if (!ft_strlen(s1) && ft_strlen(s2))
+		return (-*s2);
+	if (ft_strlen(s1) && !ft_strlen(s2))
+		return (*s1);
 	return (ft_strncmp(s1, s2, ft_strlen(s1)));
 }
