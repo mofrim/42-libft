52ab1c5a5217c4b0ff056e8337760566ab894855
mofrim <mofrim@posteo.de>
2025-01-06 08:37:34 +0100

    add comment to ft_isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,10 +6,11 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 18:03:27 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/06/06 19:47:08 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/01/06 08:37:29 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* If arg is a digit return somehting > 0. Else: 0. */
 int	ft_isdigit(int c)
 {
 	if (48 <= c && c <= 57)
