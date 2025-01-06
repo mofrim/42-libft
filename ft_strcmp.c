9e51c2343fb6ede3db8bc1c8d280fbfcc58b821c
mofrim <mofrim@posteo.de>
2025-01-06 08:23:13 +0100

    fix strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 11:35:26 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/12/05 18:05:20 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/01/06 08:23:08 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,28 @@
  * are equal. */
 int	ft_strcmp(char	*s1, char *s2)
 {
+	int	len1;
+	int	len2;
+
 	if (!s1 && !s2)
 		return (0);
 	if ((!s1 && s2) || (s1 && !s2))
 		return (1);
-	if (!ft_strlen(s1) && ft_strlen(s2))
-		return (-*s2);
-	if (ft_strlen(s1) && !ft_strlen(s2))
-		return (*s1);
-	return (ft_strncmp(s1, s2, ft_strlen(s1)));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (len1 < len2)
+	{
+		if (!ft_strncmp(s1, s2, len1))
+			return (-*(s2 + len1));
+		else
+			return (ft_strncmp(s1, s2, len1));
+	}
+	if (len1 > len2)
+	{
+		if (!ft_strncmp(s1, s2, len2))
+			return (*(s1 + len2));
+		else
+			return (ft_strncmp(s1, s2, len2));
+	}
+	return (ft_strncmp(s1, s2, len1));
 }
